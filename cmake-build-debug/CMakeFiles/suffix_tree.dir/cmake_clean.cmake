0da48edd3cbbec937ac9358b54b411beae90c935
Sergey <besmilev@gmail.com>
2022-10-05 19:30:33 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/suffix_tree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/suffix_tree.dir/main.cpp.obj"
+  "libsuffix_tree.dll.a"
+  "suffix_tree.exe"
+  "suffix_tree.exe.manifest"
+  "suffix_tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/suffix_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
